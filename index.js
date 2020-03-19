2c25d708e8a65c5cdd4177619412bc89b875360a
zzzsosleepy <zzzsojeffrey@gmail.com>
2020-03-19 05:07:49 -0300

    lotta work

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 let scrollArrow = document.getElementById("arrow");
+let whoArrow = document.getElementById("whoarrow");
+let skillsArrow = document.getElementById("skillsarrow");
 let secondContent = document.getElementById("secondContent");
+let thirdContent = document.getElementById("thirdContent");
+let fourthContent = document.getElementById("fourthContent");
 let skillsBtn = document.getElementById("skillsBtn");
+let aboutmeBtn = document.getElementById("aboutmeBtn");
+let projectsBtn = document.getElementById("projectsBtn");
 
 scrollArrow.addEventListener("click", function() {
   secondContent.scrollIntoView();
 });
 
-skillsBtn.addEventListener("click", function() {
+aboutmeBtn.addEventListener("click", function() {
   secondContent.scrollIntoView();
+});
+
+skillsBtn.addEventListener("click", function() {
+  thirdContent.scrollIntoView();
+});
+
+projectsBtn.addEventListener("click", function() {
+  fourthContent.scrollIntoView();
+});
+
+whoArrow.addEventListener("click", function() {
+  thirdContent.scrollIntoView();
+});
+skillsArrow.addEventListener("click", function() {
+  fourthContent.scrollIntoView();
 });
--- skillsChart.js
+++ skillsChart.js
@@ -1,24 +1,78 @@
 let ctx2 = document.getElementById("myChart").getContext("2d");
-let chartColors = ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"];
+//let chartColors = ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"];
+let chartColors = [
+  "#37c866",
+  "#2da276",
+  "#2c8d7a",
+  "#2b9787",
+  "#2b736a",
+  "#25635b",
+  "#234156"
+];
+let data = [95, 85, 80, 75, 73, 70, 35];
+data = data.sort(function(a, b) {
+  return b - a;
+});
+
 let chart = new Chart(ctx2, {
   // The type of chart we want to create
-  type: "horizontalBar",
+  type: "bar",
 
   // The data for our dataset
   data: {
-    labels: ["HTML", "CSS", "Javascript", "Photoshop", "React"],
+    labels: [
+      "HTML",
+      "Photoshop",
+      "CSS",
+      "Javascript",
+      "C#",
+      "Electron",
+      "React"
+    ],
     datasets: [
       {
+        order: 0,
+        barPercentage: 0.5,
+        barThickness: 75,
+        maxBarThickness: 200,
+        minBarLength: 5,
         backgroundColor: chartColors,
-        borderColor: "rgba(255,255,255,0.3)",
-        data: [80, 75, 75, 80, 25]
+        borderColor: "lightgray",
+        borderWidth: "3",
+        borderSkipped: "bottom",
+        data: data
       }
     ]
   },
 
   // Configuration options go here
   options: {
+    tooltips: {
+      enabled: false
+    },
     legend: { display: false },
-    responsive: true
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            display: false,
+            min: 0,
+            max: 100
+          }
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontFamily: "Cabin",
+            fontColor: "gray",
+            fontSize: "24"
+          },
+          gridLines: {
+            display: false
+          }
+        }
+      ]
+    }
   }
 });
