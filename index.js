982bf586f4802c25bdf38399e95e749c8019322d
zzzsosleepy <zzzsojeffrey@gmail.com>
2020-03-21 02:02:34 -0300

    projects section

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let instaBtn = document.getElementById("instagramBtn");
 let githubBtn = document.getElementById("githubBtn");
 let linkedBtn = document.getElementById("linkedBtn");
 
+let demoCTABtn = document.getElementById("demoCTABtn");
+
 scrollArrow.addEventListener("click", function() {
   secondContent.scrollIntoView();
 });
@@ -46,6 +48,8 @@ instaBtn.addEventListener("click", function() {
 linkedBtn.addEventListener("click", function() {
   window.open("https://www.linkedin.com/in/jeffrey-chipman-1a4289172/");
 });
+
+demoCTABtn.addEventListener("click", function() {});
 var a = ["dog", "cat"];
 a[100] = "fox";
 console.log("a");
