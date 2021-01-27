cdffd28df476d03f70c7edee151ff934eb935f79
zzzsosleepy <zzzsojeffrey@gmail.com>
2021-01-26 21:25:57 -0400

    Updated

--- dairy.js
+++ dairy.js
@@ -6,17 +6,20 @@ var searchBtn = document.getElementById('searchBtn');
 
 searchBtn.addEventListener("click", SearchFood)
 
-function SearchFood(){
+function SearchFood() {
     const searchParam = searchBox.value;
-    if (searchParam == ""){
+    if (searchParam == "") {
         return;
     };
     var newurl = url + searchParam;
     req.open('GET', newurl, false);
     req.setRequestHeader('Accept', "application/json");
-    req.setRequestHeader("Ocp-Apim-Subscription-Key","7c422abe4d30476e9462ed842948b421")
+    req.setRequestHeader("Ocp-Apim-Subscription-Key", "7c422abe4d30476e9462ed842948b421")
     req.send(null);
-    console.log(req.responseText);
+    var myData = JSON.parse(req.responseText);
+    for(var i = 0; i < myData.length; i++){
+        console.log(myData[i]);
+    }
 }
 
 
