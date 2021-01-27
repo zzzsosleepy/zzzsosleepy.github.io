7b817f1b9e8429ff0bc27b229610887d42920eae
zzzsosleepy <zzzsojeffrey@gmail.com>
2021-01-26 21:34:30 -0400

    Update

--- dairy.js
+++ dairy.js
@@ -12,7 +12,7 @@ function SearchFood() {
         return;
     };
     var newurl = url + searchParam;
-    req.open('GET', newurl, false);
+    req.open('GET', newurl);
     req.setRequestHeader('Accept', "application/json");
     req.setRequestHeader("Ocp-Apim-Subscription-Key", "7c422abe4d30476e9462ed842948b421")
     req.onload = function() {
