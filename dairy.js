4f5407c85b2c9ce9e8f4c15c344cdf96c213abee
zzzsosleepy <zzzsojeffrey@gmail.com>
2021-01-30 16:36:26 -0400

    Jquery + scroll animations

--- dairy.js
+++ dairy.js
@@ -6,6 +6,7 @@ var searchBtn = document.getElementById('searchBtn');
 
 var resultsDiv = document.getElementById('results');
 
+
 searchBtn.addEventListener("click", function () {
     const searchParam = searchBox.value;
     if (searchParam == "") {
@@ -33,10 +34,10 @@ function LogData(data){
             if (data[i].characteristics.includes("Dairy Free")) {
                 colorType = "greenListItem";
                 //<div class="listItem"><p>This is a test</p></div>
-                htmlString += "<div class='listItem " + colorType + "'><p>" + data[i].product + " - " + data[i].description + "<br />DAIRY FREE " + "<i class='checkmark fas fa-check-square'></i></p></div>";
+                htmlString += "<div class='listItem module " + colorType + "'><p>" + data[i].product + " - " + data[i].description + "<br />DAIRY FREE " + "<i class='checkmark fas fa-check-square'></i></p></div>";
             } else {
                 colorType = "redListItem";
-                htmlString += "<div class='listItem " + colorType + "'><p>" + data[i].product + " - " + data[i].description + "<br />CONTAINS DAIRY " + "<i class='cancel fas fa-times-circle'></i> </p></div>";
+                htmlString += "<div class='listItem module " + colorType + "'><p>" + data[i].product + " - " + data[i].description + "<br />CONTAINS DAIRY " + "<i class='cancel fas fa-times-circle'></i> </p></div>";
             }
         } else {
             console.log("No characteristics for " + data[i]);
@@ -47,6 +48,58 @@ function LogData(data){
         resultsDiv.insertAdjacentHTML('beforeend', htmlString)
     }
 }
+
+(function($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+  
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  })(jQuery);
+  
+  var win = $(window);
+  
+  var allMods = $(".module");
+  
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("already-visible"); 
+    } 
+  });
+  
+  win.scroll(function(event) {
+    
+    allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("come-in"); 
+      } 
+    });
+    
+  });
 
 
 
