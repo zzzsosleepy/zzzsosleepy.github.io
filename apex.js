367de0afe2a5d24067daa2ec3d787183cb22e47d
Jeffrey Chipman <zzzsojeffrey@gmail.com>
2021-03-22 01:13:40 -0300

    Commented code

--- apex.js
+++ apex.js
@@ -1,5 +1,8 @@
 // yQIQPeqeq9RnbCh7s507
+// https://apexlegendsapi.com/
 // https://api.mozambiquehe.re/maprotation?auth=YOURAPIKEY
+
+//Declare variables
 let currentMap = document.getElementById("currentMap");
 let nextMap = document.getElementById("nextMap");
 let timeLeft = document.getElementById("timeLeft");
@@ -15,28 +18,25 @@ let hour = 0;
 let min = 0;
 let sec = 0;
 
+//Instantly get our map info from the api
 GetMapInfo();
 
+//Fetch JSON data from the API, and call functions to update the HTML when the data is received
 function GetMapInfo(){
-    // Replace ./data.json with your JSON feed
     fetch(url)
-    .then((response) => {
-    return response.json()
-    })
+    .then((response) => { return response.json() })
     .then((data) => {
-    // Work with JSON data here
-    console.log(data)
-    console.log(data.current.map);
-    SetCurrentMap(data.current.map);
-    SetNextMap(data.next.map);
-    SetTimeLeft(data.current.remainingTimer);
-    SetImages(data.current.map, data.next.map);
+        console.log(data)
+        console.log(data.current.map);
+        SetCurrentMap(data.current.map);
+        SetNextMap(data.next.map);
+        SetTimeLeft(data.current.remainingTimer);
+        SetImages(data.current.map, data.next.map);
     })
-    .catch((err) => {
-    // Do something for an error here
-    })
+    .catch((err) => { console.log(err); })
 }
 
+//Set the background images according the current and next maps
 function SetImages(current, next){
     switch(current){
         case "Kings Canyon":
@@ -62,12 +62,17 @@ function SetImages(current, next){
     }
 }
 
+//Set current map name
 function SetCurrentMap(current){
     currentMap.innerText = current;
 }
+
+//Set next map name
 function SetNextMap(next){
     nextMap.innerText = next;
 }
+
+//Set the time left counter
 function SetTimeLeft(time){
     hour = time.slice(0,2);
     mins = time.slice(3,5);
@@ -76,6 +81,7 @@ function SetTimeLeft(time){
     timeLeft.innerText = hour + ":" + mins + ":" + sec;
 }
 
+//Subtract one second from the counter
 function SubtractSeconds(){
     if (sec - 1 <= -1){
         sec = 60;
@@ -84,5 +90,9 @@ function SubtractSeconds(){
     sec -= 1;
     timeLeft.innerText = hour + ":" + mins + ":" + sec;
 }
+
+//Subtract from the timer every second
 setInterval(SubtractSeconds, 1000);
+
+//Fetch JSON data from the API every 6 seconds
 setInterval(GetMapInfo, 6000);
