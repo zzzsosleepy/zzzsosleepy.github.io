4b7b332d83689a47907ef4fb0559364c968592b6
Jeffrey Chipman <zzzsojeffrey@gmail.com>
2021-10-27 16:57:31 -0300

    Commented entire codebase

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
+// INITIATE ANIMATE ON SCROLL
 AOS.init({
-     duration: 1200,
-     disable: 'mobile',
- })
+  duration: 1200,
+  disable: 'mobile',
+})
 
- let homeSection = document.getElementById("home");
- let aboutSection = document.getElementById("about");
- let skillsSection = document.getElementById("skills");
- let projectsSection = document.getElementById("projects");
+//  STORE SECTIONS IN VARIABLES
+let homeSection = document.getElementById("home");
+let aboutSection = document.getElementById("about");
+let skillsSection = document.getElementById("skills");
+let projectsSection = document.getElementById("projects");
 
- let navHomeBtn = document.getElementById("navHomeBtn");
- let navAboutBtn = document.getElementById("navAboutBtn");
- let navSkillsBtn = document.getElementById("navSkillsBtn");
- let navProjectsBtn = document.getElementById("navProjectsBtn");
+//  STORE NAV BUTTONS IN VARIABLES
+let navHomeBtn = document.getElementById("navHomeBtn");
+let navAboutBtn = document.getElementById("navAboutBtn");
+let navSkillsBtn = document.getElementById("navSkillsBtn");
+let navProjectsBtn = document.getElementById("navProjectsBtn");
 
- let btn_list = [];
-    btn_list.push(navHomeBtn, navAboutBtn, navSkillsBtn, navProjectsBtn);
+// CREATE AN ARRAY OF ALL NAV BUTTONS
+let btn_list = [];
+btn_list.push(navHomeBtn, navAboutBtn, navSkillsBtn, navProjectsBtn);
 
- navHomeBtn.addEventListener("click", () => ScrollToSection(homeSection));
- navAboutBtn.addEventListener("click", () => ScrollToSection(aboutSection));
- navSkillsBtn.addEventListener("click", () => ScrollToSection(skillsSection));
- navProjectsBtn.addEventListener("click", () => ScrollToSection(projectsSection));
+// ADD LISTENERS TO ALL NAV BUTTONS
+navHomeBtn.addEventListener("click", () => ScrollToSection(homeSection));
+navAboutBtn.addEventListener("click", () => ScrollToSection(aboutSection));
+navSkillsBtn.addEventListener("click", () => ScrollToSection(skillsSection));
+navProjectsBtn.addEventListener("click", () => ScrollToSection(projectsSection));
 
- function ScrollToSection(sectionName){
-    sectionName.scrollIntoView({ 
-        behavior: 'smooth'
-    });
- }
+// SCROLLS THE PAGE SMOOTHLY TO A SPECIFIC SECTION
+function ScrollToSection(sectionName) {
+  sectionName.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
 
+//SET UP AND DISPLAY LOADING SCREEN 
 const loader = document.querySelector(".loader");
-window.onload = function(){
-  setTimeout(function(){
+window.onload = function () {
+  setTimeout(function () {
     loader.style.opacity = "0";
     loader.style.pointerEvents = "none";
     btn_list.forEach((btn, i) => {
@@ -37,8 +44,8 @@ window.onload = function(){
         btn.style.transform = "translateX(0px)";
       }, i * 100);
     });
-    setTimeout(function(){
+    setTimeout(function () {
       loader.style.display = "none";
     }, 1700);
-  },1800);
+  }, 1800);
 }
