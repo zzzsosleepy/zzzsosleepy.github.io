1401d57ef0ee7d4a6ab55d2691a9418b9187d064
Jeffrey Chipman <zzzsojeffrey@gmail.com>
2021-12-02 18:08:23 -0400

    Added back to top button

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ AOS.init({
   disable: 'mobile',
 })
 
+//Get the back-to-top button:
+let backToTopButton = document.getElementById("myBtn");
+
 //  STORE SECTIONS IN VARIABLES
 let homeSection = document.getElementById("home");
 let aboutSection = document.getElementById("about");
 let skillsSection = document.getElementById("skills");
 let projectsSection = document.getElementById("projects");
 
+let accentColor = "#ffd700";
 //  STORE NAV BUTTONS IN VARIABLES
 let navHomeBtn = document.getElementById("navHomeBtn");
 let navAboutBtn = document.getElementById("navAboutBtn");
@@ -26,11 +30,41 @@ navAboutBtn.addEventListener("click", () => ScrollToSection(aboutSection));
 navSkillsBtn.addEventListener("click", () => ScrollToSection(skillsSection));
 navProjectsBtn.addEventListener("click", () => ScrollToSection(projectsSection));
 
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    backToTopButton.style.opacity = 100;
+  } else {
+    backToTopButton.style.opacity = 0;
+  }
+  if (document.body.scrollTop > 1800 && document.body.scrollTop < 2800 || document.documentElement.scrollTop > 1800 && document.documentElement.scrollTop < 2800) {
+    backToTopButton.style.backgroundColor = "rgb(233, 233, 233)";
+    backToTopButton.style.color = "#273036";
+  } else if (document.body.scrollTop < 400 || document.documentElement.scrollTop < 400) {
+    // AT TOP
+    backToTopButton.style.backgroundColor = "#273036";
+    backToTopButton.style.color = "rgb(233, 233, 233)";
+  } else if (document.body.scrollTop > 2800 || document.documentElement.scrollTop > 2800) {
+    backToTopButton.style.backgroundColor = "#273036";
+    backToTopButton.style.color = "#273036";
+  }
+}
+
 // SCROLLS THE PAGE SMOOTHLY TO A SPECIFIC SECTION
 function ScrollToSection(sectionName) {
   sectionName.scrollIntoView({
     behavior: 'smooth'
   });
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  })
 }
 
 //SET UP AND DISPLAY LOADING SCREEN 
