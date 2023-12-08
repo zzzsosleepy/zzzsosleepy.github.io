5f99bdaa200047ddc01c889b31026fbf4e79253d
Jeffrey <zzzsojeffrey@gmail.com>
2023-12-08 03:27:27 -0400

    Updated falsenet

--- falsenet/app.js
+++ falsenet/app.js
@@ -4,11 +4,11 @@ AOS.init({
     disable: 'mobile',
 })
 
-// Get references to the buttons and sections
-const homeBtn = document.getElementById("home-btn");
-const aboutBtn = document.getElementById("about-btn");
-const featuresBtn = document.getElementById("features-btn");
-const contactBtn = document.getElementById("contact-btn");
+// Get references to the buttons & sections
+const homeBtns = document.querySelectorAll(".home-btn");
+const aboutBtns = document.querySelectorAll(".about-btn");
+const featuresBtns = document.querySelectorAll(".features-btn");
+const contactBtns = document.querySelectorAll(".contact-btn");
 const logoBtn = document.getElementById("logo");
 
 const homeSection = document.getElementById("home");
@@ -16,26 +16,53 @@ const aboutSection = document.getElementById("about");
 const featuresSection = document.getElementById("features");
 const contactSection = document.getElementById("contact");
 
+// Burger button
+const burger = document.getElementById("burger-btn");
+const burgerList = document.querySelector(".burger-menu");
+const burgerCloseBtn = document.querySelector("#burger-close");
+const burgerLinks = document.querySelectorAll(".burger-link");
+
+// Add click event listener to the burger button
+burger.addEventListener("click", () => {
+    toggleBurgerMenu();
+});
+
+// Add click event listener to the burger close button
+burgerCloseBtn.addEventListener("click", () => {
+    toggleBurgerMenu();
+});
+
+// Close the burger menu when a link is clicked
+burgerLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        toggleBurgerMenu();
+    });
+});
+
+// Function to toggle the burger menu
+function toggleBurgerMenu() {
+    burgerList.classList.toggle("active");
+}
+
 // Add click event listeners to the buttons
 logoBtn.addEventListener("click", () => {
     scrollToSection(homeSection);
 });
 
-homeBtn.addEventListener("click", () => {
-    scrollToSection(homeSection);
-});
+// Helper function to add event listeners to a list of buttons
+function addClickListener(buttons, section) {
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            scrollToSection(section);
+        });
+    });
+}
 
-aboutBtn.addEventListener("click", () => {
-    scrollToSection(aboutSection);
-});
-
-featuresBtn.addEventListener("click", () => {
-    scrollToSection(featuresSection);
-});
-
-contactBtn.addEventListener("click", () => {
-    scrollToSection(contactSection);
-});
+// Add click event listeners to the buttons
+addClickListener(homeBtns, homeSection);
+addClickListener(aboutBtns, aboutSection);
+addClickListener(featuresBtns, featuresSection);
+addClickListener(contactBtns, contactSection);
 
 // Function to scroll to a section
 function scrollToSection(section) {
